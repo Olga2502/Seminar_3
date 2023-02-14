810024439a8bf4ab6e1f96fd512f9783ab0555b6
Olga <101519229+Olga2502@users.noreply.github.com>
2023-02-14 22:11:33 +0300

    Решение 18 задачи

---
+++ EX18/Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу, которая по заданному номеру 
+//четверти, показывает диапазон возможных координат 
+//точек в этой четверти (x и y).
+
+Console.Clear();
+Console.WriteLine("Ведите номер четверти :");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a == 1 || a == 4)
+{
+    Console.WriteLine("x > 0");
+    if ( a == 1)
+    {
+        Console.WriteLine("y > 0");
+    }
+    else
+    {
+        Console.WriteLine("y < 0");     
+    }
+}
+else if (a == 3)
+{
+    Console.WriteLine(" x < 0 , y < 0");
+}
+else if (a == 2)
+{
+    Console.WriteLine(" x < 0 , y > 0");
+}
+else
+{
+    Console.WriteLine("Такой четверти нет");
+}
