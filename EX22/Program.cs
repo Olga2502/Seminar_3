7d4ed0d981c7d6c0bbecb99c708683134ecc4c8d
Olga <101519229+Olga2502@users.noreply.github.com>
2023-02-15 22:41:02 +0300

    Решение задачи 22

---
+++ EX22/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход 
+//число (N) и выдаёт таблицу квадратов чисел 
+//от 1 до N.
+
+Console.Clear();
+
+int count = 1;
+Console.WriteLine("Введите число a :");
+int a = int.Parse(Console.ReadLine()!);
+
+while(count <= a)
+{
+    Console.Write(count * count);
+    if(count != a)
+    {
+        Console.Write(" , ");
+    }
+    count ++;
+}
+
